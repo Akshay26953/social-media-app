3df6d51825d4868f04019ddd9d4d80e1b3782abf
Akshay26953 <amkharat2695@gmail.com>
2022-08-30 04:38:44 +0530

    Updated client folder

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 // index.js is starting point of server side application
 
 //importing 
-import Express from "express";
+import express from "express";
 import mongoose from "mongoose";
 import cors from 'cors';
 import bodyParser from "body-parser";
@@ -16,9 +16,14 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
 //db config
-
+const CONNECTION_URL = "mongodb+srv://akshaykh1:8898413303@cluster0.vpq91gx.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 7000;
+mongoose.connect(CONNECTION_URL)
+.then(()=>{console.log("DB connected successfully")})
+.catch((err)=> {console.log(err, "Error in DB connction")});
 
 //api router
 
 
 // listen
+app.listen(PORT, ()=> {console.log(`Server running at port: ${PORT}`)})
